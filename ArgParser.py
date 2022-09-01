f358c0102ef2552b2512204b06932a7d118bc424
RonzioM <mirko.ronzio@gmail.com>
2022-09-01 16:25:23 +0200

    fix help

--- ArgParser.py
+++ ArgParser.py
@@ -6,10 +6,11 @@ def dloptions():
     parser = argparse.ArgumentParser(description='EBDl encode bed downloader')
     parser.add_argument('-o', '--organism', dest='organism', default='Homo+sapiens', help='organism, e.g. Homo+sapiens')
     parser.add_argument('-O', '--outdir', dest='outdir', help='output folder')
-    parser.add_argument('-g', '--genome', dest='gen', default='*', help='genome, e.g. hg19, GRCh38, mm10 etc...')
+    parser.add_argument('-g', '--genome', dest='gen', default='*', help='genome, e.g. hg19, GRCh38, mm10 etc...,'
+        'default all the ones available')
     parser.add_argument('-e', '--exp-type', dest='exp_type', required=True, help='experiment type: either "TF" or "Histone"')
     parser.add_argument('-f', '--factor', dest='exp', default='*', help='target name')
-    parser.add_argument('-l', '--cell-line',default='*', dest='cl', help='cell line')
+    parser.add_argument('-l', '--cell-line',default='*', dest='cl', help='cell line, default all the ones available')
     parser.add_argument('-j', '--jaspar-download', dest='jd', action='store_true',
                         help='download jaspar matrices(ignored for histones)')
     parser.add_argument('-t', '--taxonomic-group', default='vertebrates', dest='tg', help='taxonomic group')
